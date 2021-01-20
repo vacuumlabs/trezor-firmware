e90984e47b55dd88790ccc043157eea2b4033adb
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2021-01-20 08:51:34 +0100

    fixup! Validate path when converting to account number

--- core/src/apps/cardano/helpers/paths.py
+++ core/src/apps/cardano/helpers/paths.py
@@ -11,7 +11,7 @@ SHELLEY_ROOT = [1852 | HARDENED, SLIP44_ID | HARDENED]
 # fmt: off
 SCHEMA_PUBKEY = PathSchema("m/[44,1852]'/coin_type'/account'/*", SLIP44_ID)
 SCHEMA_ADDRESS = PathSchema("m/[44,1852]'/coin_type'/account'/[0,1,2]/address_index", SLIP44_ID)
-SCHEMA_ACCOUNT = PathSchema("m/[44,1852]'/coin_type'/account'", SLIP44_ID)
+SCHEMA_ACCOUNT_LOOSE = PathSchema("m/[44,1852]'/coin_type'/**", SLIP44_ID)
 # staking is only allowed on Shelley paths with suffix /2/0
 SCHEMA_STAKING = PathSchema("m/1852'/coin_type'/account'/2/0", SLIP44_ID)
 # fmt: on
